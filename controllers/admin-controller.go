a257674834e63c7cbaa3ab0586b516f7cd64454b
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-03-02 22:28:39 +0530

    changes

--- controllers/admin-controller.go
+++ controllers/admin-controller.go
@@ -1,5 +1,18 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"time"
 
-func SignUpAdmin(c *fiber.Ctx)
+	"github.com/gofiber/fiber/v2"
+	"github.com/parshwanath-p2493/Project/database"
+	"github.com/parshwanath-p2493/Project/models"
+)
+
+func SignUpAdmin(c *fiber.Ctx) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var user models.Admin
+	defer cancel()
+	collection:=database.OpenCollection("Admin")
+	_,err:=
+}
---
+++ helpers/passwords.go
@@ -0,0 +1,5 @@
+package helpers
+
+func HashPassword(password string) (string, error) {
+	return ""
+}
