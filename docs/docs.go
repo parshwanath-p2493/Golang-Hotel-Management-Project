f50934acc07b258574967c0d9ca5634910547236
Parshwanath Paramagond <parshwanathparamagond1234@gmail.com>
2025-04-02 00:06:31 +0530

    changes

--- docs/docs.go
+++ docs/docs.go
@@ -3,34 +3,7 @@ package docs
 
 import "github.com/swaggo/swag"
 
-const docTemplate = `{
-    "schemes": {{ marshal .Schemes }},
-    "swagger": "2.0",
-    "info": {
-        "description": "{{escape .Description}}",
-        "title": "{{.Title}}",
-        "contact": {},
-        "version": "{{.Version}}"
-    },
-    "host": "{{.Host}}",
-    "basePath": "{{.BasePath}}",
-    "paths": {}
-}`
-
-// SwaggerInfo holds exported Swagger Info so clients can modify it
-var SwaggerInfo = &swag.Spec{
-	Version:          "",
-	Host:             "",
-	BasePath:         "",
-	Schemes:          []string{},
-	Title:            "",
-	Description:      "",
-	InfoInstanceName: "swagger",
-	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
-}
-const docTemplate = `{
+const docTemplate =`{
     "schemes": {{ marshal .Schemes }},
     "swagger": "2.0",
     "info": {
@@ -205,6 +178,21 @@ const docTemplate = `{
         }
     }
 }`
+
+// SwaggerInfo holds exported Swagger Info so clients can modify it
+var SwaggerInfo = &swag.Spec{
+	Version:          "",
+	Host:             "",
+	BasePath:         "",
+	Schemes:          []string{},
+	Title:            "",
+	Description:      "",
+	InfoInstanceName: "swagger",
+	SwaggerTemplate:  docTemplate,
+	LeftDelim:        "{{",
+	RightDelim:       "}}",
+}
+
 func init() {
 	swag.Register(SwaggerInfo.InstanceName(), SwaggerInfo)
 }
